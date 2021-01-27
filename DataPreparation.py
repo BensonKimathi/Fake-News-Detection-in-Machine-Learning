5076d81b56e2d0b61797ca71c00590a20a299b4c
Benson Kimathi <37022584+BensonKimathi@users.noreply.github.com>
2021-01-27 14:22:20 +0300

    Working

--- DataPreparation.py
+++ DataPreparation.py
@@ -1,139 +1,59 @@
-# %load DataPreparation.py
-#importing of libraries
-import pandas as pd #Data processing, CSV file
-import numpy as numpy #Linear algebra
-import seaborn as sns #For making plots
-import nltk
-from nltk.stem import SnowballStemmer
-from nltk.stem.porter import PorterStemmer
-from nltk.tokenize import word_tokenize
+# import libraries
+import numpy as np 
+import pandas as pd 
 
-#Load the data
-test_filename = 'test.csv'
-train_filename = 'train.csv'
-valid_filename = 'valid.csv'
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 
-train_data = pd.read_csv(train_filename)
-# test_data = pd.read_csv(test_filename)
-# valid_data = pd.read_csv(valid_filename)
+# Load Datasets
+true=pd.read_csv('datasets/True.csv')
+fake=pd.read_csv('datasets/Fake.csv')
+scraped=pd.read_csv('datasets/scraped_data.csv')
 
-"""ROOT ='/datasets/'
+# Create column with labels
+fake["label"] = "fake"
+true["label"] = "true"
+scraped["label"] = scraped["site"].apply(lambda x: "fake" if(x == "breitbart" or x == "infowars" or x == "theonion") else "true")
 
-train_data = pd.read_csv(ROOT+'train.csv')
-test_data = pd.read_csv(ROOT+'test.csv')
-valid_data = pd.read_csv(ROOT+'valid.csv')
+#Rename published column with date
+scraped.rename(columns={"published": "date"}, inplace=True) 
 
-"""
+# Drop Labels
+scraped.drop(labels=["link","site"], axis=1, inplace=True)
+true.drop(labels=["subject"], axis=1, inplace=True)
 
-#Looking at the data - the data format and types
+#True news has location and publisher, which fake doesn't have. Remove for consistency
+true["text"] = true['text'].apply(lambda x: x.partition("-")[2])
 
-def data_info(): #function to hold data information
-    print(train_data.info())
-#     print(test_data.info())
-#     print(valid_data.info())
+# Change date column to datetime
+true['date'] = pd.to_datetime(true['date'].str.replace(',', ''))
+true["date"] = pd.to_datetime(true['date'], format='%Y-%m-%d', utc=False)
+fake['date'] = pd.to_datetime(fake['date'].str.replace(',', ''))
+fake["date"] = pd.to_datetime(fake['date'], format='%Y-%m-%d', utc=False)
+scraped["date"] = pd.to_datetime(true.date, format='%Y-%m-%d', utc=False)
 
-data_info()
+#Combine scraped with current datasets
+scraped_t = scraped[scraped["label"] == "true"]
+scraped_f = scraped[scraped["label"] == "fake"]
+true = pd.concat([true,scraped_t], axis=0, ignore_index=True)
+fake = pd.concat([fake,scraped_f], axis=0, ignore_index=True)
 
-#Taking a look at the data
-def data_obs():
-    print(train_data.head())
-    # print(test.head())
-    # print(valid_data.head())
+# Combine both datasets to one
+true_fake = pd.concat([true, fake]).reset_index(drop = True)
 
-data_obs()
+# Combine Title with Text
+true_fake['combined'] = true_fake['title'] + ' ' + true_fake['text']
 
-# ----- EXPLORING DATA ----- #
-#Checking for Class Imbalance
-def create_distribution(dataFile):
-    sns.set_style('whitegrid')
-    return sns.countplot(x='Label', data=dataFile)
+# Set target values as 'fake'=0 and 'true'=1
+true_fake["target"] = true_fake["label"].apply(lambda x: 1 if(x == "true") else 0)
 
-create_distribution(train_data)
-#create_distribution(test_data)
-#create_distribution(valid_data)
+# Drop rows with null values
+true_fake.dropna(subset=['text', 'date'],inplace=True)
 
-#Checking for Data Quality (Missing values)
-def data_quality():
-    print("Checking for data qualities...")
-    train_data.isnull().sum()
-    train_data.info()
+#Shuffle the dataset
+true_fake.sample(frac=1)
 
-    # test_data.isnull().sum()
-    # test_data.info()
-
-    # valid_data.isnull().sum()
-    # valid_data.info()
-
-data_quality()
-# The training dataset does not have missiong values therfore cleaning is not required
-
-#Checking for Text content 
-#Put the label column on a list to check the first 10 texts
-def text_content():
-    tweet = train_data["Statement"].to_list()
-    for i in range (5):
-        print('Tweet Number '+str(i+1)+': '+tweet[i])
-text_content()
-
-# ----- FINDING IMPORTANT WORDS ----- #
-#Stemming
-
-eng_stemmer = SnowballStemmer('english')
-stopwords = set(nltk.corpus.stopwords.words('english'))
-
-def stem_tokens(tokens, stemmer):
-    stemmed = []
-    for token in tokens:
-        stemmed.append(stemmer.stem(token))
-    return stemmed
-
-#Processing the data
-def process_data(data, exclude_stopword=True,stem=True):
-    tokens = [w.lower() for w in data]
-    tokens_stemmed = tokens
-    tokens_stemmed = stem_tokens(tokens, eng_stemmer)
-    tokens_stemmed = [w for w in tokens_stemmed if w not in stopwords]
-    return tokens_stemmed
-
-#N-grams are the fusion of multiple letters or multiple words. 
-#They are formed in such a way that even the previous and next words are captured
-
-#Unigrams
-def create_unigram(words):
-    assert type(words) == list
-    return words
-
-#Bigrams
-def create_bigrams(words):
-    assert type(words) == list
-    skip = 0
-    join_str = " "
-    Len = len(words)
-    if Len > 1:
-        lst = []
-        for i in range(Len-1):
-            for k in range(1,skip+2):
-                if i+k < Len:
-                    lst.append(join_str.join([words[i],words[i+k]]))
-    else: #Set it as unigram
-        lst = create_unigram(words)
-    return lst
-
-#trigrams
-def create_trigrams(words):
-    assert type(words) == list
-    skip = 0
-    join_str = " "
-    Len = len(words)
-    if Len > 2:
-        lst = []
-        for i in range(1,skip+2):
-            for k1 in range(1, skip+2):
-                for k2 in range(1,skip+2):
-                    if i+k1 < Len and i+k1+k2 < Len:
-                        lst.append(join_str.join([words[i],words[i+k1],words[i+k2]]))
-                        # lst.append(join_str.join([words[i], words[i+k1],words[i+k1+k2])])
-        else:
-            #set is as bigram
-            lst = create_bigram(words)
-    return lst
+# Save the cleaned and ready-to-use dataset to a new CSV
+true_fake = pd.DataFrame(true_fake) 
+true_fake.to_csv(r'C:\Users\Benson\Documents\New FakeNewsDetection\DetectFakeNews\datasets\true_fake_data.csv', index=False) 
--- FeatureSelection.py
+++ FeatureSelection.py
@@ -1,94 +1,31 @@
-"""
-
-"""
-
 import DataPreparation
-import pandas as import pd
-import numpy as np
-
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidTransformer
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-from sklearn.pipeline import Pipeline
 
 import nltk
+import re
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize,sent_tokenize
-from gensim.models.word2vec import Word2Vec
+import gensim
+from gensim.utils import simple_preprocess
+from gensim.parsing.preprocessing import STOPWORDS
 
-#Create the transform
-vectorizer = CountVectorizer()
-#Tokenizing #Encoding the transformed document
-train_vector = vectorizer.fit_transform(DataPreparation.train_data['Statement'])
+stop_words = stopwords.words('english')
+stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+def preprocess(text):
+    result = []
+    for token in gensim.utils.simple_preprocess(text):
+        if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 2 and token not in stop_words:
+            result.append(token)
+            
+    return result
 
-print (vectorizer)
-print (train_vector)
+# Using Title for Prediction
+trueFake = DataPreparation.true_fake
+trueFake['clean_title'] = DataPreparation.true_fake['title'].apply(preprocess)
+trueFake['clean_joined_title']=trueFake['clean_title'].apply(lambda x:" ".join(x))
 
-#Document term matrix
-def get_Vectorizer_stats():
-    #Vocabulary size
-    train_vector.shape
+# Using Text for Prediction
+trueFake['clean_text'] = DataPreparation.true_fake['text'].apply(preprocess)
+trueFake['clean_joined_text']=trueFake['clean_text'].apply(lambda x:" ".join(x))
 
-    #Check the vocabulary
-    print(vectorizer.vocabulary_)
-
-#TF-IDF Features
-tfidf = TfidTransformer()
-train_tfidf = tfidf.fit_transform(train_vector)
-
-def get_tfidf_stats():
-     #Vocabulary size
-    train_tfidf.shape
-
-    #Check the vocabulary
-    print(train_tfidf.A[:10])
-
-#Bag of words - ngrams
-countV_ngram = CountVectorizer(ngram_range=(1,3),stop_words='english')
-tfidf_ngram = TfidTransformer(use_idf=True,smooth_idf=True)
-
-tfidf_ngram = TfidfVectorizer(stop_words='english',ngram_range=(1,4),use_idf=True,smooth_idf=True)
-
-#Part Of Speech (POS) Tagging
-tagged_sentences = nltk.corpus.treebank.tagged_sents()
-
-cutoff = int(.75 *len(tagged_sentences))
-training_sentences = DataPreparation.train_data['Statement']
-
-print(training_sentences)
-
-"""
-stop_words = set(stopwords.words('english'))
-# Tokenize the text
-tokens = sent_tokenize(DataPreparation.train_data['Statement'])
-#Generate tagging for all the tokens using loop
-for i in tokens:    
-    words = nltk.word_tokenize(i)    
-    words = [w for w in words if not w in stop_words]    
-#  POS-tagger.    
-tags = nltk.pos_tag(words)
-tags
-"""
-#Training POS tagger based on words
-def features(sentence, index):
-    """ sentence: [w1,w2, ...], index: the index of the word """
-    return {
-        'word': sentence[index],
-        'is_first': index == 0,
-        'is_last': [index == len(sentence)-1,
-        'is_capitalized': sentence[index][0].upper() == sentence[index][0],
-        'is_all_caps': sentence[index].upper() == sentence[index],
-        'is_all_lower': sentence[index].lower() == sentence[index],
-        'capitals_inside': sentence[index][1:].lower()!=sentence[index][1:],
-        'prefix-1': sentence[index][0],
-        'prefix-2': sentence[index][:2],
-        'prefix-3': sentence[index][:3],
-        'suffix-1': sentence[index][-1],
-        'suffix-2': sentence[index][-2:],
-        'suffix-3': sentence[index][-3:],
-        'prev_word': '' if index == 0 else sentence[index-1],
-        'next_word': '' if index == len(sentence)-1 else sentence[index+1],
-        'has_hyphen': '-' in sentence[index],
-        'is_numeric': sentence[index].isdgit()
-    }
+# Using Joined Title and Text
+trueFake['clean_final'] = DataPreparation.true_fake['combined'].apply(preprocess)
+trueFake['clean_joined_final']=trueFake['clean_final'].apply(lambda x:" ".join(x))
